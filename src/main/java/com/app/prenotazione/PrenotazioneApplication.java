81d4b9a0c8bdb0537cab8b34828f510abf494286
Melero <jfernandezmelero@safareyes.es>
2022-09-27 18:40:52 +0200

    1 subida

---
+++ src/main/java/com/app/prenotazione/PrenotazioneApplication.java
@@ -0,0 +1,15 @@
+package com.app.prenotazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class PrenotazioneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrenotazioneApplication.class, args);
+    }
+
+}
